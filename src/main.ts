90ba178ae90f9f6a2f6170f2b29e761b114e28f4
Bablu Ahmed <bablukpik@gmail.com>
2023-10-28 20:16:48 +0600

    main: initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Product Data Importer')
+    .setDescription('The NestJS Prisma Product Manager')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // App port
+  const configService = app.get(ConfigService);
+  const port = configService.get('APP_PORT');
+
+  await app.listen(port || 3000);
+  console.info(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
